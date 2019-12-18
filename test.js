7b35908699ed9ed588f130672628cc57da972a79
spriteololo <30690634+spriteololo@users.noreply.github.com>
2019-12-18 23:47:13 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,2 @@
 var hostname_oil = /http:\/\/(.+)\//.exec(document.getElementsByTagName("script")[document.getElementsByTagName("script").length - 1].src);
-hostname_oil = hostname_oil[1]; //TODO
 console.log(hostname_oil)
