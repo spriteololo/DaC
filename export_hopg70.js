86301ad5bab24fb8019072d54ba13d633d1442c4
Khilkovich Dzmitry <dimakhilkovich@gmail.com>
2023-10-27 10:31:42 +0200

    [ttt] v7.0 forest

--- export_hopg70.js
+++ export_hopg70.js
@@ -2347,7 +2347,7 @@ var addObs = function () {
             if (buttons == 1) { // активировать кнопки
                 buttons = 0;
                 Indicator("lawngreen", "B5");
-                AddJS(1, "export_hopg69.js");
+                AddJS(1, "export_hopg70.js");
             }
         }
         if (OnOffguard == 1) {
@@ -2355,7 +2355,7 @@ var addObs = function () {
                 guard = 0;
                 guard_act = 1;
                 Indicator("lawngreen", "G");
-                AddJS(1, "export_hopg69.js");
+                AddJS(1, "export_hopg70.js");
             }
         }
     } // end-fight
